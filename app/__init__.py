cebc89169870f852999e4bdcf5358f31c567d24d
soohian <shonendumm@gmail.com>
2024-07-28 21:49:23 +0800

    Add blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
+from .blueprints import main
 
 app = Flask(__name__)
 
+app.register_blueprint(main)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 if __name__ == '__main__':
