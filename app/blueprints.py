7799a551d6261007e0554c07d85ed0953c7c5aa9
soohian <shonendumm@gmail.com>
2024-07-28 22:27:10 +0800

    Fix error with cache dict by removing app.extensions

--- app/blueprints.py
+++ app/blueprints.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, current_app
 
+
 main = Blueprint('main', __name__)
 
 @main.route('/')
@@ -9,14 +10,12 @@ def index():
 @main.route('/add/<request>')
 def add(request):
     from app import cache
-    # cache = current_app.extensions['cache']
-    # breakpoint()
+    
     cache.set("value", request)
     return f"value added: {request}"
 
 @main.route('/get')
 def getvalue():
     from app import cache
-    # cache = current_app.extensions['cache']
     value = cache.get("value")
     return f"hello, value is {value}"
